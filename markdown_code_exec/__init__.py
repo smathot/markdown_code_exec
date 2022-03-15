38d98a52b79e956b4f09179424a4cc040e98f827
smathot <s.mathot@cogsci.nl>
2022-03-15 18:19:03 +0100

    Prepare for packaging!

--- markdown_code_exec/__init__.py
+++ markdown_code_exec/__init__.py
@@ -1,3 +1,5 @@
-__version__ = '0.2.0'
+"""Executes Python code in Markdown files and captures the output"""
+
+__version__ = '0.3.0'
 
 from markdown_code_exec._markdown_code_exec import parse_file, parse_text
